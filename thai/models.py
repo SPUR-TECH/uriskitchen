fcb6978cc1806773c552d09889940e3714274572
Scott Atkinson <scott.atkin79@gmail.com>
2022-04-14 17:55:21 +0000

    create superuser and first model

--- thai/models.py
+++ thai/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Meal(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    price_Medium = models.DecimalField(max_digits=5, decimal_places=2)
+    price_Large = models.DecimalField(max_digits=5, decimal_places=2)
+    featured_image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.title
+        
