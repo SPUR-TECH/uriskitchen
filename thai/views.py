02062acd6ab0fc0c2b82f631eb14db0d0608a750
Scott Atkinson <scott.atkin79@gmail.com>
2022-04-23 15:18:48 +0000

    add active to selected pages

--- thai/views.py
+++ thai/views.py
@@ -4,14 +4,15 @@ from .models import Meal, Home, Desert
 
 def home_view(request):
     template = "thai/index.html"
-    return render(request, template)
+    context = {'active_link': 'index'}
+    return render(request, template, context)
 
 
 def meal_view(request):
     template = "thai/meal.html"
     meal_objects = Meal.objects.all()
     context = {
-        'meal_objects': meal_objects}
+        'meal_objects': meal_objects, 'active_link': 'meal'}
     return render(request, template, context)
 
 
@@ -19,5 +20,5 @@ def desert_view(request):
     template = "thai/desert.html"
     desert_objects = Desert.objects.all()
     context = {
-        'desert_objects': desert_objects}
+        'desert_objects': desert_objects,'active_link': 'desert'}
     return render(request, template, context)
