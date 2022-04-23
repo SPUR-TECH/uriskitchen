c55bd82ebcf752d5823e73b30c3e0cd4bc8cd3b6
Scott Atkinson <scott.atkin79@gmail.com>
2022-04-23 14:20:45 +0000

    start styling meals page

--- thai/views.py
+++ thai/views.py
@@ -12,7 +12,6 @@ def meal_view(request):
     meal_objects = Meal.objects.all()
     context = {
         'meal_objects': meal_objects}
-    print(meal_objects)
     return render(request, template, context)
 
 
