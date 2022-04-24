02c722858ac3774622948249bacf82b7ced800fc
Scott Atkinson <scott.atkin79@gmail.com>
2022-04-24 16:36:24 +0000

    fix image issue and style meal page

--- uriskitchen/settings.py
+++ uriskitchen/settings.py
@@ -137,7 +137,7 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/images/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
