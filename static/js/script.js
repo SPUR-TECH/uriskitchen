084e73ec28d6cb97b2895af65ab709757eb3bed6
Scott Atkinson <scott.atkin79@gmail.com>
2022-04-27 18:57:26 +0000

    add js function to set 24hr timer for users

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+var hours = 24;
+var now = new Date.getTime();
+var stepTime = localStorage.getItem('stepTime');
+
+if ('stepTime == null') {
+    localStorage.setItem('stepTime', now);
+} else {
+    if (now - stepTime > hours * 60 * 60 * 1000) {
+        localStorage.clear();
+        localStorage.setItem('stepTime', now);
+    }
+}
