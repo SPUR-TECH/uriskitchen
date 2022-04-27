6a8f51f470eb3d22c31a074106a31d2cb3e1af72
Scott Atkinson <scott.atkin79@gmail.com>
2022-04-27 19:45:47 +0000

    add and style order list and add names to meal id to select radio button

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,12 @@
-var hours = 24;
-var now = new Date.getTime();
-var stepTime = localStorage.getItem('stepTime');
+// var hours = 24;
+// var now = new Date.getTime();
+// var stepTime = localStorage.getItem('stepTime');
 
-if ('stepTime == null') {
-    localStorage.setItem('stepTime', now);
-} else {
-    if (now - stepTime > hours * 60 * 60 * 1000) {
-        localStorage.clear();
-        localStorage.setItem('stepTime', now);
-    }
-}
+// if ('stepTime == null') {
+//     localStorage.setItem('stepTime', now);
+// } else {
+//     if (now - stepTime > hours * 60 * 60 * 1000) {
+//         localStorage.clear();
+//         localStorage.setItem('stepTime', now);
+//     }
+// }
