06599528845ac4206d220c0865b06ec2dd14f4f3
Scott Atkinson <scott.atkin79@gmail.com>
2022-04-30 15:26:33 +0000

    add all info to order list

--- static/js/order.js
+++ static/js/order.js
@@ -8,5 +8,17 @@ function addMeal(mid) {
     // Get name of meal
 
     var name = document.querySelector('#mealname' + mid).innerHTML;
-    mealcart.innerHTML += '<li>' + name + '</li>';
+    //  Get meal prices
+    var radio = 'meal' + mid;
+    var pri = document.getElementsByName(radio);
+    var size, price;
+    if (pri[0].checked) {
+        price = pri[0].value;
+        size = 'M';
+    } else {
+        price = pri[1].value;
+        size = 'L'
+    }
+
+    mealcart.innerHTML += '<li>' + size + ' :   ' + name + ' £' + price + '</li>';
 }
