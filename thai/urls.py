dd9ae05288b847ed44dec21be354ebb63005a054
Scott Atkinson <scott.atkin79@gmail.com>
2022-05-15 15:08:29 +0000

    re add all meals and desserts to database do to database error

--- thai/urls.py
+++ thai/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.home_view, name='home_view'),
     path('meal/', views.meal_view, name='meal_view'),
-    path('desert/', views.desert_view, name='desert_view'),
+    path('dessert/', views.dessert_view, name='dessert_view'),
     path('cart/', views.cart_view, name='cart_view'),
     path('signup/', views.signup_view, name='signup_view'),
     path('login/', views.login_view, name='login_view'),
