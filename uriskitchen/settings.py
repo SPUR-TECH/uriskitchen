fb0f14c0bc4847d1eb1e5718f77c81d9018e0202
Scott Atkinson <scott.atkin79@gmail.com>
2022-05-24 14:53:08 +0000

    remove debug code from settings as it didnt work

--- uriskitchen/settings.py
+++ uriskitchen/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DEBUG'))
+DEBUG = False
 
 
 ALLOWED_HOSTS = ["uriskitchen.herokuapp.com", "localhost"]
